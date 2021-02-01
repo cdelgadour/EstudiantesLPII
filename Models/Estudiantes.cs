6509c9cb6a6aa1dcd231bbcfd50fe3c64ad5dead
DERIAN AGRAMONTE GOMEZ <da16-2484@365.unphu.edu.do>
2021-02-01 19:45:47 -0400

    Edit2

---
+++ Models/Estudiantes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EstudiantesLPII.Models
+{
+    public class Estudiantes
+    {
+		public int ID_Estudiante { get; set; }
+		public string Nombre_Estudiante { get; set; }
+		public string Apellido_Estudiante { get; set; }
+		public int Matricula_Estudiante { get; set; }
+		public int Carrera_Estudiante { get; set; }
+		public int Estatus_Estudiante { get; set; }
+	}
+}
