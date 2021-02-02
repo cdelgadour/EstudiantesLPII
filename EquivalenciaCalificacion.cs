115af33170b0ba06fc871f7600b840726dcd3ecd
pdavidsalomon <78319614+pdavidsalomon@users.noreply.github.com>
2021-02-02 18:56:49 -0400

    Add files via upload

---
+++ EquivalenciaCalificacion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EstudiantesLPII
+{
+    public class EquivalenciaCalificacion
+    {
+        public int Id { get; set; }
+        public int rangoInicial { get; set; }
+        public int rangoFinal { get; set; }
+        public int literal { get; set; }
+        public int valorCalcIndice { get; set; }
+
+    }
+}
---
+++ Localidad.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EstudiantesLPII
+{
+    public class Localidad
+    {
+        public int Id { get; set; }
+        public string nombre { get; set; }
+        public string aula { get; set; }
+        public int piso { get; set; }
+    }
+}
